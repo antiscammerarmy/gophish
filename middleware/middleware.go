c4c8639c528ed136765585870b01824245bb1725
thetrentus <thetrentus@users.noreply.github.com>
2017-10-03 12:48:46 +0800

    Update
    Take all files from the official repository and put them into the clone

---
+++ middleware/middleware.go
@@ -0,0 +1,109 @@
+package middleware
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gophish/gophish/auth"
+	ctx "github.com/gophish/gophish/context"
+	"github.com/gophish/gophish/models"
+	"github.com/gorilla/csrf"
+)
+
+var CSRFExemptPrefixes = []string{
+	"/api",
+}
+
+func CSRFExceptions(handler http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, prefix := range CSRFExemptPrefixes {
+			if strings.HasPrefix(r.URL.Path, prefix) {
+				r = csrf.UnsafeSkipCheck(r)
+				break
+			}
+		}
+		handler.ServeHTTP(w, r)
+	}
+}
+
+// GetContext wraps each request in a function which fills in the context for a given request.
+// This includes setting the User and Session keys and values as necessary for use in later functions.
+func GetContext(handler http.Handler) http.HandlerFunc {
+	// Set the context here
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Parse the request form
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing request", http.StatusInternalServerError)
+		}
+		// Set the context appropriately here.
+		// Set the session
+		session, _ := auth.Store.Get(r, "gophish")
+		// Put the session in the context so that we can
+		// reuse the values in different handlers
+		r = ctx.Set(r, "session", session)
+		if id, ok := session.Values["id"]; ok {
+			u, err := models.GetUser(id.(int64))
+			if err != nil {
+				r = ctx.Set(r, "user", nil)
+			} else {
+				r = ctx.Set(r, "user", u)
+			}
+		} else {
+			r = ctx.Set(r, "user", nil)
+		}
+		handler.ServeHTTP(w, r)
+		// Remove context contents
+		ctx.Clear(r)
+	}
+}
+
+func RequireAPIKey(handler http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		ak := r.Form.Get("api_key")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+			w.Header().Set("Access-Control-Max-Age", "1000")
+			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+			return
+		}
+		if ak == "" {
+			JSONError(w, 400, "API Key not set")
+			return
+		} else {
+			u, err := models.GetUserByAPIKey(ak)
+			if err != nil {
+				JSONError(w, 400, "Invalid API Key")
+				return
+			}
+			r = ctx.Set(r, "user_id", u.Id)
+			r = ctx.Set(r, "api_key", ak)
+			handler.ServeHTTP(w, r)
+		}
+	}
+}
+
+// RequireLogin is a simple middleware which checks to see if the user is currently logged in.
+// If not, the function returns a 302 redirect to the login page.
+func RequireLogin(handler http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if u := ctx.Get(r, "user"); u != nil {
+			handler.ServeHTTP(w, r)
+		} else {
+			http.Redirect(w, r, "/login", 302)
+		}
+	}
+}
+
+// JSONError returns an error in JSON format with the given
+// status code and message
+func JSONError(w http.ResponseWriter, c int, m string) {
+	cj, _ := json.MarshalIndent(models.Response{Success: false, Message: m}, "", "  ")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(c)
+	fmt.Fprintf(w, "%s", cj)
+}
---
+++ models/result.go
@@ -0,0 +1,112 @@
+package models
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"net/mail"
+
+	"github.com/jinzhu/gorm"
+	"github.com/oschwald/maxminddb-golang"
+)
+
+type mmCity struct {
+	GeoPoint mmGeoPoint `maxminddb:"location"`
+}
+
+type mmGeoPoint struct {
+	Latitude  float64 `maxminddb:"latitude"`
+	Longitude float64 `maxminddb:"longitude"`
+}
+
+// Result contains the fields for a result object,
+// which is a representation of a target in a campaign.
+type Result struct {
+	Id         int64   `json:"-"`
+	CampaignId int64   `json:"-"`
+	UserId     int64   `json:"-"`
+	RId        string  `json:"id"`
+	Email      string  `json:"email"`
+	FirstName  string  `json:"first_name"`
+	LastName   string  `json:"last_name"`
+	Position   string  `json:"position"`
+	Status     string  `json:"status" sql:"not null"`
+	IP         string  `json:"ip"`
+	Latitude   float64 `json:"latitude"`
+	Longitude  float64 `json:"longitude"`
+}
+
+// UpdateStatus updates the status of the result in the database
+func (r *Result) UpdateStatus(s string) error {
+	return db.Table("results").Where("id=?", r.Id).Update("status", s).Error
+}
+
+// UpdateGeo updates the latitude and longitude of the result in
+// the database given an IP address
+func (r *Result) UpdateGeo(addr string) error {
+	// Open a connection to the maxmind db
+	mmdb, err := maxminddb.Open("static/db/geolite2-city.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mmdb.Close()
+	ip := net.ParseIP(addr)
+	var city mmCity
+	// Get the record
+	err = mmdb.Lookup(ip, &city)
+	if err != nil {
+		return err
+	}
+	// Update the database with the record information
+	return db.Table("results").Where("id=?", r.Id).Updates(map[string]interface{}{
+		"ip":        addr,
+		"latitude":  city.GeoPoint.Latitude,
+		"longitude": city.GeoPoint.Longitude,
+	}).Error
+}
+
+// GenerateId generates a unique key to represent the result
+// in the database
+func (r *Result) GenerateId() error {
+	// Keep trying until we generate a unique key (shouldn't take more than one or two iterations)
+	const alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	k := make([]byte, 7)
+	for {
+		for i := range k {
+			idx, err := rand.Int(rand.Reader, big.NewInt(int64(len(alphaNum))))
+			if err != nil {
+				return err
+			}
+			k[i] = alphaNum[idx.Int64()]
+		}
+		r.RId = string(k)
+		err := db.Table("results").Where("r_id=?", r.RId).First(&Result{}).Error
+		if err == gorm.ErrRecordNotFound {
+			break
+		}
+	}
+	return nil
+}
+
+// Returns the email address to use in the "To" header of the email
+func (r *Result) FormatAddress() string {
+	addr := r.Email
+	if r.FirstName != "" && r.LastName != "" {
+		a := &mail.Address{
+			Name:    fmt.Sprintf("%s %s", r.FirstName, r.LastName),
+			Address: r.Email,
+		}
+		addr = a.String()
+	}
+	return addr
+}
+
+// GetResult returns the Result object from the database
+// given the ResultId
+func GetResult(rid string) (Result, error) {
+	r := Result{}
+	err := db.Where("r_id=?", rid).First(&r).Error
+	return r, err
+}
